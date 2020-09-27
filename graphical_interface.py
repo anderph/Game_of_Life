8f0c91b887da2c05717ba58e38aa3f5bc5436d6d
anderph <37943772+anderph@users.noreply.github.com>
2020-09-27 01:47:25 -0400

    added graphical interface for game which includes an optional grid, a graphical representation of the matrix and the ability to select and deselect cells.

--- graphical_interface.py
+++ graphical_interface.py
@@ -1,29 +1,108 @@
 # This is the Graphical Interface file.
 #
 # Here, I will allow the user to see the grid, change the grid size, go forward and back generations
-
-
+import numpy as np
 import PySimpleGUI as sg
+import matrixManipulation as mm
+import math
 
-sg.theme('DarkAmber')   # Add a little color to your windows
+visibleMatrix = np.zeros((2, 2))  # 2,2 is arbitrary, used to assign it as a matrix type
 
-layout = [ [sg.Text('Test For GUI')],
-           [sg.Graph((400,400), (0,0), (400,400), key='GRAPH')],
-           [sg.Text('Enter command:'), sg.InputText()],
-           [sg.OK(), sg.Cancel()]]
+# default variable definition
+size_of_grid = 768
+cell_width = 25
+cells_to_fit = math.floor(size_of_grid / cell_width / 2) * 2
+grid_line_width = 1
+
+matrix_x_position = 15
+matrix_y_position = 15
+
+
+# Function Definitions:
+
+def visible_matrix():
+    global visibleMatrix
+    x = matrix_x_position
+    y = matrix_y_position
+    offset = round(cells_to_fit / 2)
+    visibleMatrix = mm.current_generation[x - offset:x + offset, y - offset:y + offset]
+    print(visibleMatrix)
+
+
+def locate_button_press():
+    offset = round(cells_to_fit / 2)
+    effective_grid_size = cells_to_fit * cell_width
+    pos_x = mouse[0]
+    pos_y = size_of_grid - mouse[1]  # inverted so that 0,0 is on top left (like matrix) instead of bottom left (graph)
+    matrix_x = round((pos_x - cells_to_fit / 2) / effective_grid_size * cells_to_fit)  # rounds to nearest cell number
+    matrix_y = round((pos_y - cells_to_fit / 2) / effective_grid_size * cells_to_fit)  # rounds to nearest cell number
+    mm.current_generation[matrix_x - offset + matrix_x_position, matrix_y - offset + matrix_y_position] = \
+            (not mm.current_generation[matrix_x, matrix_y]) * 1
+                                                # flips the nearest
+    print(matrix_y)
+
+def draw_matrix():
+    visible_matrix()
+    live_cells = np.where(visibleMatrix == 1)
+    print(live_cells)
+    for i in range(len(live_cells[0])):
+        x = live_cells[0][i]
+        y = live_cells[1][i]
+
+        top_left = ((x * cell_width), size_of_grid - (y * cell_width))
+        bot_right = ((x * cell_width + cell_width), size_of_grid - (y * cell_width + cell_width))
+        graph.DrawRectangle(top_left, bot_right, fill_color='red', line_color='red')
+
+
+def draw_grid():
+    for i in range(cells_to_fit):
+        x = i
+        for p in range(cells_to_fit):
+            y = p
+
+            top_left = ((x * cell_width), size_of_grid - (y * cell_width))
+            bot_right = ((x * cell_width + cell_width), size_of_grid - (y * cell_width + cell_width))
+            graph.DrawRectangle(top_left, bot_right, line_color='black')
+
+
+def print_GUI_info():
+    print('cell width: ' + str(cell_width))
+    print('cells_to_fit: ' + str(cells_to_fit))
+    print('grid_line_width: ' + str(grid_line_width))
+
+
+print_GUI_info()
+
+sg.theme('DarkAmber')  # background color
+
+layout = [[sg.Graph((size_of_grid, size_of_grid), (0, 0), (size_of_grid, size_of_grid), key='GRAPH',
+                    change_submits=True, drag_submits=False)],
+          [sg.Text('Enter command:'), sg.InputText()],
+          [sg.OK(), sg.Cancel()]]
 
 window = sg.Window('My new window').Layout(layout)
 graph = window.Element('GRAPH')
 
-
 event, values = window.Read()
 
-
-while True:             # Event Loop
+while True:  # Event Loop
     event, values = window.Read()
     if event is None:
         break
-    for i in range(1,10):
-        top_left = (i*25, i*25)
-        bot_right = (i*25+20, i*25-20)
-        graph.DrawRectangle(top_left, bot_right, fill_color='red', line_color='red')
+    if event in (sg.WIN_CLOSED, 'Exit'):
+        break
+
+    graph.erase()
+    draw_matrix()
+    draw_grid()
+    mm.next_generation()
+
+    mouse = values['GRAPH']
+
+    if event == 'GRAPH':
+        if mouse == (None, None):
+            continue
+        locate_button_press()
+        graph.erase()
+        draw_matrix()
+        draw_grid()
--- matrixManipulation.py
+++ matrixManipulation.py
@@ -1,24 +1,25 @@
 import numpy as np
 
+# TODO: right now you will get an error once something reaches the edge of the matrix. \
+#  write something that will clear the edges of the matrix (maybe the last 10 or so rows)
 
 
-
-matrix_size = 7  # defines the size of the matrix
+matrix_size = 50  # defines the size of the matrix
 current_generation = np.zeros((matrix_size, matrix_size))  # sets up the matrix (full of zeroes)
 
 # creates an empty matrix that will count the neighbours for each cell
 neighbor_count = np.zeros((matrix_size, matrix_size))
 
-to_live = np.empty([1,1])
-to_die = np.empty([1,1])
-to_be_born = np.empty([1,1])
+to_live = np.empty([1, 1])
+to_die = np.empty([1, 1])
+to_be_born = np.empty([1, 1])
 print(to_die)
 gen_saver = []
-
 
 current_generation[1, 1] = 1
 current_generation[1, 2] = 1
 current_generation[1, 3] = 1
+current_generation[29, 29] = 1
 print(str(current_generation))
 
 
@@ -26,7 +27,7 @@ def create_neighbour_matrix():
     global neighbor_count
     neighbor_count = np.zeros((matrix_size, matrix_size))
     live_cells = np.where(current_generation == 1)
-    print(live_cells)
+    # print(live_cells)
     for pos in range(len(live_cells[0])):
         for col in range(-1, 2):  # when added to the x position, will be x-1, x , and x + 1
             for row in range(-1, 2):  # when added to the y position will be y-1, y, and y + 1
@@ -38,13 +39,16 @@ def create_neighbour_matrix():
 
 
 create_neighbour_matrix()
-print(neighbor_count)
+
+
+# print(neighbor_count)
 
 
 def next_generation():
+    create_neighbour_matrix()  # calculates the neighbours
     global current_generation
     # to_die = np.where(neighbor_count <= 1 or neighbor_count >=4)
-        # commented out because it will die without haaving to check this since it won't live/be born
+    # commented out because it will die without haaving to check this since it won't live/be born
     to_live = np.where(neighbor_count == 2)
     to_be_born = np.where(neighbor_count == 3)
     gen_saver.append(current_generation)
@@ -55,12 +59,9 @@ def next_generation():
     for cell in range(len(to_live[0])):  # keeps cells alive if they are live previously
         if current_generation[to_live[0][cell], to_live[1][cell]] == 1:
             interim_matrix[to_live[0][cell], to_live[1][cell]] = 1
-    for cell in range(len(to_be_born)):  # births all new cells + those who are alive with 3 neighbors
+    for cell in range(len(to_be_born[0])):  # births all new cells + those who are alive with 3 neighbors
         interim_matrix[to_be_born[0][cell], to_be_born[1][cell]] = 1
     current_generation = interim_matrix
     # print(to_live)
     # print(to_be_born)
-    print(current_generation)
-
-
-next_generation()
+    # print(current_generation)
