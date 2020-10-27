dc0b48f6d8b07ef235542dbe9ed6e4a04d9f8bbe
anderph <37943772+anderph@users.noreply.github.com>
2020-10-27 13:13:42 -0400

    Update graphical_interface.py

--- graphical_interface.py
+++ graphical_interface.py
@@ -22,7 +22,7 @@ matrix_y_position = 500
 
 # GUI initialization
 
-sg.theme('DarkAmber')  # background color
+# sg.theme('DarkAmber')  # background color
 
 layout = [[sg.Graph((size_of_grid, size_of_grid), (0, 0), (size_of_grid, size_of_grid), key='GRAPH',
                     change_submits=True, drag_submits=False)],
